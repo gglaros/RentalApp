175e2b69fd76aed68dedcbe5c13c9a221687a04c
Giorgos <giorgos2532018@yahoo.com>
2025-12-18 20:52:32 +0200

    Remove frontend app folder

--- frontend/app/database/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.database.models.users import User
-from app.database.models.ownerApplication import OwnerApplication
-from app.database.models.property import Property
-from app.database.models.revokedToken import RevokedToken
