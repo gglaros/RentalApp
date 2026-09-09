c69a1635e6d165f0e47ae30b8f00e164f4e78ee3
Giorgos <giorgos2532018@yahoo.com>
2026-09-09 14:20:16 +0300

    Remove duplicate app folder

--- app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-# app/api/__init__.py
-from flask import Blueprint
-from app.api.errors import register_error_handlers
-
-def create_api_blueprint():
-    bp = Blueprint("api", __name__)
-    register_error_handlers(bp)
-    return bp
